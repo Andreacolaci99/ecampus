83fd0a78706d17def333b663da2842d5d5f59f73
Andreacolaci99 <colaciandrea99@gmail.com>
2026-01-12 20:34:22 +0100

    second commit

--- src/main/java/mado/id/ecampus/controller/homeController.java
+++ src/main/java/mado/id/ecampus/controller/homeController.java
@@ -18,7 +18,46 @@ public class homeController {
     @GetMapping
     public String home(Model model) {
         model.addAttribute("servizi", servizirepo.findAll());
+        model.addAttribute("pageTitle", "MADO • Polo e-Campus");
         return "homepage";
     }
 
+    @GetMapping("/chisiamo")
+    public String chiSiamo(Model model) {
+        model.addAttribute("pageTitle", "Chi siamo • MADO");
+        return "chisiamo";
+    }
+
+    @GetMapping("/contatti")
+    public String contatti(Model model) {
+        model.addAttribute("pageTitle", "Contatti • MADO");
+        return "contatti";
+    }
+
+    @GetMapping("/corsi")
+    public String corsi(Model model) {
+        model.addAttribute("servizi", servizirepo.findAll());
+        model.addAttribute("pageTitle", "Corsi • MADO");
+        return "corsi";
+    }
+
+    @GetMapping("/lingue")
+    public String certificazioniLingue() {
+        return "subpages/cert-lingue";
+    }
+
+    @GetMapping("/eipass")
+    public String eipass() {
+        return "subpages/eipass";
+    }
+
+    @GetMapping("/ecampus")
+    public String ecampus() {
+        return "subpages/ecampus";
+    }
+
+     @GetMapping("/link")
+    public String link() {
+        return "subpages/link";
+    }
 }
